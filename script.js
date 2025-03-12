47062cbfda341ce47405b772f734fa35bebbf994
Alexandra McVay <alya.mcvay@gmail.com>
2025-03-11 17:59:34 -0700

    added css

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function downloadVCard() {
     window.location.href = 'download_vcard.php';
 }
+
+function getCurrentYear() {
+  return new Date().getFullYear();
+}
+
+document.getElementById("currentYear").innerHTML = getCurrentYear();
