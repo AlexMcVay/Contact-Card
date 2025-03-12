abb04b6cfe8e1d850ecdefd39bce2c36206f7d52
Alexandra McVay <alya.mcvay@gmail.com>
2025-03-11 18:12:54 -0700

    reformat

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function downloadVCard() {
-    window.location.href = 'download_vcard.php';
-}
+// Set the current year in the footer.
+document.getElementById("currentYear").textContent = new Date().getFullYear();
 
-function getCurrentYear() {
-  return new Date().getFullYear();
-}
+document.getElementById("currentYear").textContent = new Date().getFullYear();
 
-document.getElementById("currentYear").innerHTML = getCurrentYear();
+// Corrected button href attributes.
+const portfolioButton = document.querySelectorAll('button')[1];
+const linkedinButton = document.querySelectorAll('button')[2];
+const githubButton = document.querySelectorAll('button')[3];
+const contactButton = document.querySelectorAll('button')[4];
+
+portfolioButton.addEventListener('click', () => {
+  window.open('https://alexmcvay.github.io', '_blank');
+});
+
+linkedinButton.addEventListener('click', () => {
+  window.open('https://www.linkedin.com/in/alexandramcvay', '_blank');
+});
+
+githubButton.addEventListener('click', () => {
+  window.open('https://github.com/alexmcvay', '_blank');
+});
+
+contactButton.addEventListener('click', () => {
+  window.location.href = 'mailto:alya.mcvay@gmail.com';
+});
